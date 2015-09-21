fdeca3fdb803f378db5a2b7fee5ae078d81adf6d
limonka <ewa.limanowka@gmail.com>
2015-09-21 10:29:41 +0200

    Fixed typos in tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,19 +5,19 @@ class UserTest < ActiveSupport::TestCase
     @user = build(:user)
   end
 
-  test "position is valid with valid attributes" do
+  test "user is valid with valid attributes" do
     assert @user.valid?
   end
 
-  test 'position is invalid if email does not exist' do
+  test 'user is invalid if email does not exist' do
     @user.email = nil
     assert @user.invalid?
     assert @user.errors[:email].present?
   end
 
-  test 'position is invalid if email is not uniq' do
-    new_user = create(:user)
-    @user.email = new_user.email
+  test 'user is invalid if email is not uniq' do
+    other_user = create(:user)
+    @user.email = other_user.email
     assert @user.invalid?
     assert @user.errors[:email].present?
   end
