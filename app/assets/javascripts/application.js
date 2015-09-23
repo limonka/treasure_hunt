92c6b01868565d902fdf14aceb77247a9e562706
limonka <ewa.limanowka@gmail.com>
2015-09-23 12:31:28 +0200

    Code cleanup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,13 @@
 //= require jquery_ujs
 //= require underscore
 //= require gmaps/google
+
+function show_gmap(markersHash) {
+  handler = Gmaps.build('Google');
+  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+    markers = handler.addMarkers(markersHash);
+    handler.bounds.extendWith(markers);
+    handler.fitMapToBounds();
+    handler.getMap().setZoom(7);
+  });
+}
