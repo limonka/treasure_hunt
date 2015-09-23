467866ed1b12fb6cd1c867319f1faa965c3645ef
limonka <ewa.limanowka@gmail.com>
2015-09-23 22:50:31 +0200

    Added test for auth token

--- test/controllers/treasure_hunt_controller_test.rb
+++ test/controllers/treasure_hunt_controller_test.rb
@@ -1,15 +1,26 @@
 require "test_helper"
 
 class TreasureHuntControllerTest < ActionController::TestCase
-  test "create should response success if everything when fine" do
+  setup do
+    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials("treasure")
+  end
+
+  test "create should response 200 if everything is fine" do
     post :create, email: 'test@treasure.com', current_position: Treasure.current_position, format: :json
 
     assert_response 200
   end
 
-  test "create should response error if something went wrong" do
+  test "create should response 422 if something is wrong" do
     post :create, email: 'treasure.com', format: :json
 
     assert_response 422
+  end
+
+  test "create without http auth returns 401" do
+    request.env['HTTP_AUTHORIZATION'] = ''
+    post :create, email: 'test@treasure.com', current_position: Treasure.current_position, format: :json
+
+    assert_response 401
   end
 end
